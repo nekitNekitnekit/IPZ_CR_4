dc317ed0da8343adf29cb0a641fb70a6e4c994ea
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 12:49:03 +0200

    Create StateUtils.kt file with navigation and content type classes

---
+++ app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/utils/StateUtils.kt
@@ -0,0 +1,11 @@
+package ua.edu.lntu.ipz_cr_4.ui.utils
+
+enum class MyAppNavigationType {
+    NO_NAVIGATION,
+    PERMANENT_NAVIGATION_DRAWER
+}
+
+enum class MyAppContentType {
+    LIST_ONLY,
+    LIST_AND_DETAIL
+}
