4a885b09a40f7d2c55384e81776f8cb5c4967d0d
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 12:18:19 +0200

    Create setCategory() and setID() functions

--- app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/MyViewModel.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/MyViewModel.kt
@@ -1,12 +1,29 @@
 package ua.edu.lntu.ipz_cr_4.ui
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class MyViewModel : ViewModel() {
-    
+
     private val _uiState = MutableStateFlow(MyUiState(categoryList = DataSource.getCategoryData()))
     val uiState: StateFlow<MyUiState> = _uiState.asStateFlow()
+
+    fun setCategory(selectedCategory: Category) {
+        _uiState.update {
+            it.copy(
+                currentCategory = selectedCategory,
+                headerTitleId = selectedCategory.titleResourceId
+            )
+        }
+        updateIDListData(selectedCategory.titleResourceId)
+    }
+
+    fun setID(selectedID: ID) {
+        _uiState.update {
+            it.copy(currentID = selectedID)
+        }
+    }
 
 }
