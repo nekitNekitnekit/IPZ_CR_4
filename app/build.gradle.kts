6f1c674bd9a8e72d39e556b5cdccafc707eabb0b
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 12:10:02 +0200

    implement libs to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.compose.material3:material3-window-size-class")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
