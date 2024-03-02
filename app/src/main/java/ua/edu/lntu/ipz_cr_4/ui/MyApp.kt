07cf8fd014a777090da9e06b49f4bbc5aeb0fb11
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 12:37:28 +0200

    Create MyApp() function

--- app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/MyApp.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/MyApp.kt
@@ -1,19 +1,29 @@
 package ua.edu.lntu.ipz_cr_4.ui
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import ua.edu.lntu.ipz_cr_4.R
 
 enum class MyScreen {
@@ -56,4 +66,56 @@ fun MyAppBar(
             }
         }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(
+    windowSize: WindowWidthSizeClass,
+    viewModel: MyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = MyScreen.valueOf(
+        backStackEntry?.destination?.route ?: MyScreen.CATEGORY.name
+    )
+
+    val uiState by viewModel.uiState.collectAsState()
+
+    Scaffold(
+        topBar = {
+            MyAppBar(
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() },
+                currentScreen = currentScreen,
+                headerResId = if (currentScreen == MyScreen.CATEGORY)
+                    R.string.app_name
+                else
+                    uiState.headerTitleId
+            )
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = MyScreen.CATEGORY.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = MyScreen.CATEGORY.name) {
+                CategoryListScreen(
+                    categoryList = uiState.categoryList,
+                    onCardClick = {
+                        viewModel.setCategory(it)
+                        navController.navigate(MyScreen.ID.name)
+                    }
+                )
+            }
+            composable(route = MyScreen.ID.name) {
+                IdScreen(
+                    recommendation = uiState.currentId
+                )
+            }
+        }
+
+    }
 }
