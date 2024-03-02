adde1854d3026d6f6cc1f074e9b05e6607df5164
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 16:29:17 +0200

    configured the back navigation button to appear only on the second screen and disappear on the first screen

--- app/src/main/java/ua/edu/lntu/ipz_cr_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cr_4/MainActivity.kt
@@ -21,14 +21,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import ua.edu.lntu.ipz_cr_4.ui.theme.IPZ_CR_4Theme
 
@@ -51,21 +54,40 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun IPZ_CR_4() {
-    val navController = rememberNavController()
+fun MyAppBar(
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(text = "Головна") },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun IPZ_CR_4(
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = backStackEntry?.destination?.route ?: "Головна"
     val number = rememberSaveable { mutableStateOf(0) }
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text(text = "Головна") },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        navController.navigate("screen1")
-                    }) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Назад")
-                    }
-                }
+            MyAppBar(
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
             )
         }
     ) { innerPadding ->
