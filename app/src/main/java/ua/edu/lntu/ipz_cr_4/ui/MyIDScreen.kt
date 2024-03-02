6c744756025f9eb7ab09f10cf5e95bf5e5e213b7
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 12:46:34 +0200

    Create MyIDScreen.kt file with IdScreen() function

---
+++ app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/MyIDScreen.kt
@@ -0,0 +1,53 @@
+package ua.edu.lntu.ipz_cr_4.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import ua.edu.lntu.ipz_cr_4.model.Id
+
+@Composable
+fun IdScreen(
+    recommendation: Id,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .padding(12.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = stringResource(id = recommendation.titleResourceId),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier
+                    .padding(bottom = 12.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
