ded5a3a3a98ae8a9609e9a4e53ab8d419f90bb26
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 13:52:58 +0200

    Create MainPage()

--- app/src/main/java/ua/edu/lntu/ipz_cr_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cr_4/MainActivity.kt
@@ -3,10 +3,14 @@ package ua.edu.lntu.ipz_cr_4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,10 +20,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -71,9 +78,29 @@ fun IPZ_CR_4(name: String, modifier: Modifier = Modifier) {
                 MainPage(navController, number)
             }
             composable("screen2") {
-                PageWithText(navController, number)
+                SecondPage(navController, number)
             }
         }
+    }
+}
+
+@Composable
+fun MainPage(navController: NavHostController, number: MutableState<Int>) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        for (i in 1 until 11) {
+            Button(onClick = {
+                number.value = i
+                navController.navigate("screen2")
+            }) {
+                Text("Назад до сторінки ${i}")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
