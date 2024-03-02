de072a88221fc39750bbe9d05ddbd2d71b5ad883
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 13:45:11 +0200

    delete data and model packages with files

--- app/src/main/java/ua/edu/lntu/ipz_cr_4/model/Category.kt
+++
@@ -1,7 +0,0 @@
-package ua.edu.lntu.ipz_cr_4.model
-
-import androidx.annotation.StringRes
-
-data class Category(
-    @StringRes val titleResourceId: Int
-)
--- app/src/main/java/ua/edu/lntu/ipz_cr_4/model/Category_id.kt
+++
@@ -1,8 +0,0 @@
-package ua.edu.lntu.ipz_cr_4.model
-
-import androidx.annotation.StringRes
-
-data class Category_id(
-    val id: Int,
-    @StringRes val idResourceId: Int
-)
