43855882e4ea346680eaaaf6ff5f069b4da4b0c4
nekitNekitnekit <klientkv0711@gmail.com>
2024-03-02 12:33:03 +0200

    Create TopAppBar

--- app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/MyApp.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cr_4/ui/MyApp.kt
@@ -1,6 +1,59 @@
 package ua.edu.lntu.ipz_cr_4.ui
 
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import ua.edu.lntu.ipz_cr_4.R
+
 enum class MyScreen {
     CATEGORY,
     ID
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyAppBar(
+    currentScreen: MyScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = {},
+    @StringRes headerResId: Int,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = headerResId),
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.onPrimary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back_button),
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+        }
+    )
 }
